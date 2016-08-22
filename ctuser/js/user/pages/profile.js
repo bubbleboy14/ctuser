df572f850e5bdcc468742f316a91f861d6feb2c8
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-21 20:43:40 -0700

    profile/results js

--- ctuser/js/user/pages/profile.js
+++ ctuser/js/user/pages/profile.js
@@ -1,13 +1,15 @@
 CT.require("CT.all");
+CT.require("core");
 CT.require("user.core");
 
 CT.onload(function() {
+	CT.initCore();
 	var u = user.core.get();
 	if (!u)
 		location = "/";
 	else if (location.hash) { // view profile
 		CT.db.one(location.hash.slice(1), function(data) {
-			CT.dom.setContent(document.body, CT.layout.profile(data));
+			CT.dom.addContent("ctmain", CT.layout.profile(user.core.prep(data)));
 		});
 	} else { // edit profile
 
---
+++ ctuser/js/user/pages/results.js
@@ -0,0 +1,15 @@
+CT.require("CT.all");
+CT.require("core");
+CT.require("user.core");
+
+CT.onload(function() {
+	CT.initCore();
+	user.core.all(function(users) {
+		new CT.slider.Slider({
+			frames: users,
+			parent: "ctmain",
+			mode: "profile",
+			defaultImg: core.config.ctuser.defaults.img
+		});
+	});
+});
