5af582705fd222ab3eff7b11e02597b458c6bda5
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-31 19:33:38 -0700

    results: support sections[]

--- ctuser/js/user/pages/results.js
+++ ctuser/js/user/pages/results.js
@@ -4,12 +4,37 @@ CT.require("user.core");
 
 CT.onload(function() {
 	CT.initCore();
-	user.core.all(function(users) {
-		new CT.slider.Slider({
-			frames: users,
-			parent: "ctmain",
-			mode: "profile",
-			defaultImg: core.config.ctuser.defaults.img
+	var cfg = core.config.ctuser;
+	if (cfg.results.sections) {
+		var results = {}, build = function() {
+			if (Object.keys(results).length != cfg.results.sections.length)
+				return;
+			new CT.slider.Slider({
+				frames: cfg.results.sections.map(function(s) {
+					return {
+						label: s.name,
+						frames: results[s.name]
+					}
+				}),
+				mode: "chunk",
+				parent: "ctmain",
+				subMode: "profile",
+				bubblePosition: "top"
+			});
+		};
+		cfg.results.sections.forEach(function(s) {
+			user.core.all(function(data) {
+				results[s.name] = data;
+				build();
+			}, null, CT.merge(s.filters, cfg.results.filters));
 		});
-	}, null, core.config.ctuser.results.filters);
+	} else {
+		user.core.all(function(users) {
+			new CT.slider.Slider({
+				frames: users,
+				parent: "ctmain",
+				mode: "profile"
+			});
+		}, null, cfg.results.filters);
+	}
 });
