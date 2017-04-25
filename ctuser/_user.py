40218706bde8251c673bf6aebf0392fc0d7c5cd1
bubbleboy14 <mario.balibrera@gmail.com>
2017-04-24 17:37:54 -0700

    /_user handler: emailer

--- ctuser/_user.py
+++ ctuser/_user.py
@@ -5,7 +5,7 @@ from model import db, CTUser, Message, Conversation
 from emailTemplates import JOIN, ACTIVATE, CONTACT
 
 def response():
-    action = cgi_get("action", choices=["join", "activate", "login", "contact", "edit"])
+    action = cgi_get("action", choices=["join", "activate", "login", "contact", "edit", "email"])
     if action == "join":
         email = cgi_get("email")
         if CTUser.query(CTUser.email == email).get():
@@ -18,7 +18,7 @@ def response():
         if config.mailer:
             usk = u.key.urlsafe()
             send_mail(to=u.email, subject="activation required",
-                body=JOIN["body"]%(usk,), html=JOIN["html"]%(usk,))
+                body=JOIN%(usk,))
         else: # auto-activate
             u.active = True
         u.put()
@@ -28,7 +28,7 @@ def response():
             u.active = True
             u.put()
         send_mail(to=u.email, subject="account activated",
-            body=ACTIVATE["body"], html=ACTIVATE["html"])
+            body=ACTIVATE)
         redirect("/", "you did it!")
     elif action == "login":
         u = CTUser.query(CTUser.email == cgi_get("email"),
@@ -53,14 +53,19 @@ def response():
             if recipient != sender.key:
                 send_mail(to=recipient.get().email,
                     subject="message from %s"%(sender.firstName,),
-                    body=CONTACT["body"]%(sender.fullName(), message,
-                        sender.firstName, sender.key.urlsafe(), conversation.key.urlsafe()),
-                    html=CONTACT["html"]%(sender.fullName(), message,
-                        sender.key.urlsafe(), sender.firstName, conversation.key.urlsafe()))
+                    body=CONTACT%(sender.fullName(), message,
+                        sender.firstName, sender.key.urlsafe(), conversation.key.urlsafe()))
         succeed(convokey and m.key.urlsafe() or conversation.key.urlsafe())
     elif action == "edit":
         changes = cgi_get("changes")
         changes["key"] = cgi_get("user")
         edit(changes)
+    elif action == "email":
+        sender = db.get(cgi_get("user"))
+        if not sender.admin:
+            fail()
+        recips = cgi_get("recipients", default=["mario.balibrera@gmail.com"])
+        for recip in recips:
+            send_mail(to=recip, subject=cgi_get("subject"), body=cgi_get("body"))
 
 respond(response)
